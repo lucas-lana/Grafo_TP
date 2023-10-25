b2b9fe934489c327cbe10f2f8522ea0e12a08e92
Lucas Lana <lucas.fonseca.lana@gmail.com>
2023-10-25 20:22:16 -0300

    Funções 2,4,7 feitas
    Além da realização das funções 2, 4 e 7, foi realizado uma reestruturação do código. Agora existindo um menu intermediário para decidir quando voltar para o menu principal. Mudança de Switch Case para eles if's devido a erros com a antiga implementação. Por fim a criação de uma função que possa receber qualquer Grafo, a partir do nome do arquivo.

--- main.py
+++ main.py
@@ -1,98 +1,138 @@
 import networkx as nx
+#import matplotlib.pyplot as plt
+from networkx import *
+import os
+import time
 
-# Cria um grafo vazio
+#Função para receber qualquer grafo.graphml
+
+#def receberGrafo():
+    # Cria um grafo vazio
+#    Grafo = nx.Graph()
+#    print("Digite o nome do arquivo que contém o grafo: ")
+#    Arquivo = input()
+#    Arquvo = str(Arquivo)
+    
+    #A variável Grafo recebe um grafo lido de um arquivo GraphML (teste.graphml)
+#    Grafo = nx.read_graphml(Arquivo)
+#    print("Grafo lido com sucesso!")
+
+
+#Mini menu para retornar para o menu principal
+def exibir_submenu():
+    while True:
+        print("\n----------//----------")
+        print("Deseja voltar para o menu anterior?")
+        print("1 - Sim")
+        print("2 - Não")
+        opcao2 = input("Digite uma opção: ")
+        opcao2 = int(opcao2)
+        if opcao2 == 1:
+            print("Você selecionou a opção 1.")
+            break
+        elif opcao2 == 2:
+            print("Você selecionou a opção 2.")
+            exit()
+        else:
+            print("Opção inválida.")
+
+
+#receberGrafo()
+
 Grafo = nx.Graph()
-
-#A variável Grafo recebe um grafo lido de um arquivo GraphML (teste.graphml)
-Grafo = nx.read_graphml('teste.graphml')
-
+Grafo = nx.read_graphml('teste.graphml')   
+print("Grafo lido com sucesso!")
 
 #Menu de opções
 while True:
-    #Continua a execução do programa até que o usuário digite a opção 14
-    
-    #Imprime o menu de opções
-    print("\n\n----MENU----\n")
-    print("\n1 - Adicionar nó")
-    print("2 - Adicionar aresta")
-    print("3 - Imprimir grafo")
-    print("4 - Retornar a ordem do grafo")
-    print("5 - Retornar o tamanho do grafo")
-    print("6 - Retornar os vizinhos de um vértice")
-    print("7 - Determinar o grau de um vértice")
-    print("8 - Retornar a sequência de graus do grafo")
-    print("9 - Determinar a excentricidade de um vértice")
-    print("10 - Determinar o raio do grafo")
-    print("11 - Determinar o diâmetro do grafo")
-    print("12 - Determinar o centro do grafo")
-    print("13 - Determinar sequencia de vertices visitados na busca em largura")
-    print("14 - Determinar distância e caminho mínimo")
-    print("15 - Determinar a proximidade C de um vértice")
-    print("16 - Sair")
-    print("----------//----------")
-    
-    
-    opcao = int(input("Digite uma opção: "))
-    
-    
-    #Estrutura de switch case
-    def case1():
-        print("Você selecionou a opção 1.")
-
-    def case2():
-        print("Você selecionou a opção 2.")
-
-    def case3():
-        print("Você selecionou a opção 3.")
-
-    def case4():
-        print("Você selecionou a opção 4.")
-
-    def case5():
-        print("Você selecionou a opção 5.")
-
-    def case6():
-        print("Você selecionou a opção 6.")
-
-    def case7():
-        print("Você selecionou a opção 7.")
-
-    def case8():
-        print("Você selecionou a opção 8.")
-
-    def case9():
-        print("Você selecionou a opção 9.")
-
-    def case10():
-        print("Você selecionou a opção 10.")
-
-    def case11():
-        print("Você selecionou a opção 11.")
-
-    def case12():
-        print("Você selecionou a opção 12.")
-
-    def case13():
-        print("Você selecionou a opção 13.")
+        #Continua a execução do programa até que o usuário digite a opção 14
         
-    def case14():
-        print("Você selecionou a opção 14.")
-    
-    def case15():
-        print("Você selecionou a opção 15.")
-    
-    def case16():
-        print("Você selecionou a opção 16.")
-        break
+        #Imprime o menu de opções
+        print("\n\n----MENU----\n")
+        print("1 - Imprimir grafo")
+        print("2 - Retornar a ordem do grafo")
+        print("3 - Retornar o tamanho do grafo")
+        print("4 - Retornar os vizinhos de um vértice")
+        print("5 - Determinar o grau de um vértice")
+        print("6 - Retornar a sequência de graus do grafo")
+        print("7 - Determinar a excentricidade de um vértice")
+        print("8 - Determinar o raio do grafo")
+        print("9 - Determinar o diâmetro do grafo")
+        print("10 - Determinar o centro do grafo")
+        print("11 - Determinar sequencia de vertices visitados na busca em largura")
+        print("12 - Determinar distância e caminho mínimo")
+        print("13 - Determinar a proximidade C de um vértice")
+        print("14 - Sair")
+        print("----------//----------")
         
-    def case_default():
-        print("Opção inválida.")
-        break
-
-    switch_case = {
-        1: case1, 2: case2, 3: case3,
-        4: case4, 5: case5, 6: case6,
-        7: case7, 8: case8, 9: case9,
-        10: case10, 11: case11, 12: case12, 13: case13,
-        14: case14, 15: case15,16: case16
-    }
+        
+        opcao = (input("\nDigite uma opção: "))
+        opcao = int(opcao)
+        
+        if opcao == 1:
+            #nx.draw(Grafo, with_labels=True, font_weight='bold')
+            print("Em desenvolvimento...")
+            exibir_submenu()
+        
+        elif opcao == 2:
+            ordem = Grafo.order()
+            print("A ordem do grafo é: ",ordem)
+            exibir_submenu()
+            
+        elif opcao == 3:
+            print("Você selecionou a opção 3.")
+            exibir_submenu()
+                
+        elif opcao == 4:
+            print ("Vértices do grafo: ", Grafo.nodes())
+            vertice = input("Digite um vértice: ")
+            vizinhos = list(Grafo.neighbors(vertice))
+            print("Os vizinhos do vértice ", vertice, " são: ", vizinhos)
+            exibir_submenu()
+                
+        elif opcao == 5:
+            print("Você selecionou a opção 5.")
+            exibir_submenu()
+            
+        elif opcao == 6:
+            print("Você selecionou a opção 6.")
+            exibir_submenu()
+            
+        elif opcao == 7:
+            print ("Vértices do grafo: ", Grafo.nodes())
+            vertice = input("Digite um vértice: ")
+            excentricidade = nx.eccentricity(Grafo, v=vertice, sp=None)
+            print("A excentricidade do vértice ", vertice, " é: ", excentricidade)
+            exibir_submenu()
+            
+        elif opcao == 8:
+            print("Você selecionou a opção 8.")
+            exibir_submenu()
+            
+        elif opcao == 9:
+            print("Você selecionou a opção 9.")
+            exibir_submenu()
+            
+        elif opcao == 10:
+            print("Você selecionou a opção 10.")
+            exibir_submenu()
+            
+        elif opcao == 11:
+            print("Você selecionou a opção 11.")
+            exibir_submenu()
+            
+        elif opcao == 12:
+            print("Você selecionou a opção 12.")
+            exibir_submenu()
+            
+        elif opcao == 13:
+            print("Você selecionou a opção 13.")
+            exibir_submenu()
+            
+        elif opcao == 14:
+            print("Você selecionou a opção 14.")
+            break
+        else:
+            print("Opção inválida.")
+            
+        time.sleep(1)
